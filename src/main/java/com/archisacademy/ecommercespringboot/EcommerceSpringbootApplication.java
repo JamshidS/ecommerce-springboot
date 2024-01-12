b01628151d7d516eaecb2af34478209baf5f9d69
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-01-12 18:37:51 +0300

    Initial commit

---
+++ src/main/java/com/archisacademy/ecommercespringboot/EcommerceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.archisacademy.ecommercespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringbootApplication.class, args);
+	}
+
+}
