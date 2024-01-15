4442224353f81001b8fea0c33c2f054a91e485aa
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-01-15 18:36:04 +0300

    [DE-04]- create enum for user role

---
+++ src/main/java/com/archisacademy/ecommercespringboot/enums/UserRole.java
@@ -0,0 +1,31 @@
+package com.archisacademy.ecommercespringboot.enums;
+
+import java.util.stream.Stream;
+
+public enum UserRole {
+    USER("user"),
+    ADMIN("admin");
+
+    private String description;
+
+    UserRole(String description){
+        this.description=description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+    public static UserRole value(String description){
+        return Stream.of(UserRole.values())
+                .filter(x -> x.getDescription().equals(description))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+}
