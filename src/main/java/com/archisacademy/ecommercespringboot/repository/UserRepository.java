e526476237a004a188e8a2b92c15b15cf8f1b800
bidoluyazılım <baranagtas1903@gmail.com>
2024-01-16 10:02:21 +0300

    [DE-4] - create entity, jpa and enum fixed

--- src/main/java/com/archisacademy/ecommercespringboot/repository/UserRepository.java
+++ src/main/java/com/archisacademy/ecommercespringboot/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.archisacademy.ecommercespringboot.repository;
 
-import com.archisacademy.ecommercespringboot.model.UserEntity;
+import com.archisacademy.ecommercespringboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-    Optional<UserEntity> findById(Long id);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUUID(String uuid);
 }
