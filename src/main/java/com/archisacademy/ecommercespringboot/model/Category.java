c76b1b3bb45af95e1f18a5ebfdda6ba924bafecb
bidoluyazılım <baranagtas1903@gmail.com>
2024-01-17 15:10:55 +0300

    [DE-6] - Category entity and jpa add

---
+++ src/main/java/com/archisacademy/ecommercespringboot/model/Category.java
@@ -0,0 +1,33 @@
+package com.archisacademy.ecommercespringboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name ="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "uuid")
+    private String uuid;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "created_at")
+    private Date createdAt;
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    @OneToMany
+    @JoinColumn(name = "product_id")
+    Product product;
+
+}
