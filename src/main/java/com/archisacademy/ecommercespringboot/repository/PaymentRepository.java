bda0f6e6a071f4c75061bf573768d06fee577eeb
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2024-01-22 14:34:23 +0300

    [DE-7] Created Payment

---
+++ src/main/java/com/archisacademy/ecommercespringboot/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.archisacademy.ecommercespringboot.repository;
+
+import com.archisacademy.ecommercespringboot.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+    Payment findByUuid(String uuid);
+}
