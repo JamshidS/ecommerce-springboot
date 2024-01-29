3152f597414d3b093ae6d775588b0ee2a482f8a9
bidoluyazılım <baranagtas1903@gmail.com>
2024-01-29 17:37:46 +0300

    [DE-12] create cart entity and jpa

---
+++ src/main/java/com/archisacademy/ecommercespringboot/model/Cart.java
@@ -0,0 +1,32 @@
+package com.archisacademy.ecommercespringboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name ="carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "order_date")
+    private Date orderDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<Refund> refunds;
+    @OneToOne
+    @JoinColumn(name = "promotion_id")
+    private Promotion promotion;
+
+}
