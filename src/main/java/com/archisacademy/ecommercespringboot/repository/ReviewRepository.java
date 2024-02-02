8a4e3dadb4c529c62007a405ca5f9865256a2046
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-02-02 18:46:50 +0300

    fix

--- src/main/java/com/archisacademy/ecommercespringboot/repository/ReviewRepository.java
+++ src/main/java/com/archisacademy/ecommercespringboot/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.archisacademy.ecommercespringboot.repository;
 
 import com.archisacademy.ecommercespringboot.model.Review;
+import com.archisacademy.ecommercespringboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +11,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findByUuid(String uuid);
 
     List<Review> findAllReviewsByUserUuid(String uuid);
+
+    List<Review> findAllByUser(User user); // uuid
+
 }
