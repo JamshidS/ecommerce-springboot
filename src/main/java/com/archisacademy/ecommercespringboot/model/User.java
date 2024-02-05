3c92207ca20863a04107e477f5f91d95a27d87f8
bidoluyazılım <baranagtas1903@gmail.com>
2024-02-05 17:07:02 +0300

    [DE-3]-Return entity

--- src/main/java/com/archisacademy/ecommercespringboot/model/User.java
+++ src/main/java/com/archisacademy/ecommercespringboot/model/User.java
@@ -54,5 +54,6 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Payment> payment;
 
-
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Return> returnList;
 }
