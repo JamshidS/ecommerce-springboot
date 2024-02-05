08dc39b10d0812892a24fb8c03ce31e72bd7c2a2
bidoluyazılım <baranagtas1903@gmail.com>
2024-02-05 15:40:55 +0300

    [DE-3]-Return entity

---
+++ src/main/java/com/archisacademy/ecommercespringboot/model/Return.java
@@ -0,0 +1,35 @@
+package com.archisacademy.ecommercespringboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name ="returns")
+public class Return {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "reason")
+    private String reason;
+    @Column(name = "return_date")
+    private Date returnDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    Cart cart;
+
+}
