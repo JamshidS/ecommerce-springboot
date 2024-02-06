9e79423a4ac37380df1e0490f1c2d48ae91fbbc3
bidoluyazılım <baranagtas1903@gmail.com>
2024-02-06 13:17:10 +0300

    [DE-3]-Problem fixed

--- src/main/java/com/archisacademy/ecommercespringboot/model/User.java
+++ src/main/java/com/archisacademy/ecommercespringboot/model/User.java
@@ -1,5 +1,6 @@
 package com.archisacademy.ecommercespringboot.model;
 
+import com.archisacademy.ecommercespringboot.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,7 +29,7 @@ public class User {
     private String password;
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role")
-    private String userRole;
+    private UserRole userRole;
     @Column(name = "telephone")
     private String telephone;
     @Column(name = "address")
