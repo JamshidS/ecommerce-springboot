08e27147bf7b3831b452fe6b21012b9a6f9bfbb2
bidoluyazılım <baranagtas1903@gmail.com>
2024-04-07 18:17:07 +0300

    update wishlist controller

--- src/main/java/com/archisacademy/ecommercespringboot/controller/WishlistController.java
+++ src/main/java/com/archisacademy/ecommercespringboot/controller/WishlistController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("wishlist")
+@RequestMapping("/wishlist")
 public class WishlistController {
     private final WishlistService wishlistService;
 
