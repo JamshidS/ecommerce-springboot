4cde982a2b54d880b874bdb7ef2d3e275b08590b
bidoluyazılım <baranagtas1903@gmail.com>
2024-04-15 18:02:50 +0300

    productdto

--- src/main/java/com/archisacademy/ecommercespringboot/mapper/impl/WishlistMapperImpl.java
+++ src/main/java/com/archisacademy/ecommercespringboot/mapper/impl/WishlistMapperImpl.java
@@ -1,17 +1,32 @@
 package com.archisacademy.ecommercespringboot.mapper.impl;
 
+import com.archisacademy.ecommercespringboot.dto.ProductDto;
 import com.archisacademy.ecommercespringboot.dto.response.WishlistResponse;
+import com.archisacademy.ecommercespringboot.mapper.ProductMapper;
 import com.archisacademy.ecommercespringboot.mapper.WishlistMapper;
 import com.archisacademy.ecommercespringboot.model.Wishlist;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class WishlistMapperImpl implements WishlistMapper {
+    private final ProductMapper productMapper; // ProductMapper injection
+
+    public WishlistMapperImpl(ProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
+
     @Override
     public WishlistResponse convertToResponse(Wishlist wishlist) {
         WishlistResponse response = new WishlistResponse();
         if (wishlist != null) {
-            response.setProductList(wishlist.getProducts());
+            List<ProductDto> productDTOs = wishlist.getProducts()
+                    .stream()
+                    .map(productMapper::toProductDto)
+                    .collect(Collectors.toList());
+            response.setProductList(productDTOs);
             response.setUserUuid(wishlist.getUuid());
         }
         return response;
