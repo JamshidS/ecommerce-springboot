f69988e3ff8ec1ea566b2311f8d808343a77b05b
Jamshid(Timur)  Sadik <77556948+JamshidS@users.noreply.github.com>
2024-04-26 18:07:15 +0300

    Update InventoryController.java

--- src/main/java/com/archisacademy/ecommercespringboot/controller/InventoryController.java
+++ src/main/java/com/archisacademy/ecommercespringboot/controller/InventoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Inventory")
+@RequestMapping("/inventory")
 public class InventoryController {
     private final InventoryService inventoryService;
 
