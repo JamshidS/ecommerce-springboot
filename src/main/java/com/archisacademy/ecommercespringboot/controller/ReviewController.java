f3090c0bd420b555545cb324412917b1a6257a54
bidoluyazılım <baranagtas1903@gmail.com>
2024-04-26 17:05:00 +0300

    [DE-28]-review controller

--- src/main/java/com/archisacademy/ecommercespringboot/controller/ReviewController.java
+++ src/main/java/com/archisacademy/ecommercespringboot/controller/ReviewController.java
@@ -20,23 +20,20 @@ public class ReviewController {
 
     @PostMapping("/save")
     public ResponseEntity<ReviewDto> saveReview(@RequestBody ReviewDto reviewDto) {
-        ReviewDto savedReview = reviewService.saveReview(reviewDto);
-        return new ResponseEntity<>(savedReview, HttpStatus.CREATED);
+        return new ResponseEntity<>(reviewService.saveReview(reviewDto), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<ReviewDto> getReviewByUserAndProduct(
             @RequestParam("userUuid") String userUuid,
             @RequestParam("productUuid") String productUuid) {
-        ReviewDto reviewDto = reviewService.getReviewByUserUuid(userUuid, productUuid);
-        return new ResponseEntity<>(reviewDto, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.getReviewByUserUuid(userUuid, productUuid), HttpStatus.OK);
     }
 
     @GetMapping("/product")
     public ResponseEntity<List<ReviewDto>> getReviewsByProduct(
             @RequestParam("productUuid") String productUuid) {
-        List<ReviewDto> reviewDtos = reviewService.getReviewByProductUuid(productUuid);
-        return new ResponseEntity<>(reviewDtos, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.getReviewByProductUuid(productUuid), HttpStatus.OK);
     }
 
     @PutMapping("/update")
@@ -44,36 +41,31 @@ public class ReviewController {
             @RequestParam("userUuid") String userUuid,
             @RequestParam("productUuid") String productUuid,
             @RequestBody ReviewDto updatedReviewDto) {
-        String message = reviewService.updateReviewByUserUuid(userUuid, productUuid, updatedReviewDto);
-        return new ResponseEntity<>(message, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.updateReviewByUserUuid(userUuid, productUuid, updatedReviewDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteReviewByUserAndProduct(
             @RequestParam("userUuid") String userUuid,
             @RequestParam("productUuid") String productUuid) {
-        String message = reviewService.deleteReviewByUserUuid(userUuid, productUuid);
-        return new ResponseEntity<>(message, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.deleteReviewByUserUuid(userUuid, productUuid), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{reviewUuid}") // deleteReviewsByReviewUuid
     public ResponseEntity<String> deleteReviewsByReviewUuid(
             @PathVariable("reviewUuid") String reviewUuid) {
-        String message = reviewService.deleteReviewsByReviewUuid(reviewUuid);
-        return new ResponseEntity<>(message, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.deleteReviewsByReviewUuid(reviewUuid), HttpStatus.OK);
     }
 
     @GetMapping("/approved")
     public ResponseEntity<List<ReviewDto>> getAllApprovedReviewsByProduct(
             @RequestParam("productUuid") String productUuid) {
-        List<ReviewDto> reviewDtos = reviewService.getAllReviewsByProductUUID(productUuid);
-        return new ResponseEntity<>(reviewDtos, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.getAllReviewsByProductUUID(productUuid), HttpStatus.OK);
     }
 
     @PutMapping("/approve")
     public ResponseEntity<String> approveReview(
             @RequestParam("reviewUuid") String reviewUuid) {
-        String message = reviewService.approveReview(reviewUuid);
-        return new ResponseEntity<>(message, HttpStatus.OK);
+        return new ResponseEntity<>(reviewService.approveReview(reviewUuid), HttpStatus.OK);
     }
 }
